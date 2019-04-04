ea2fb4041dd69d0b54fb589e1387c56c5d7fe30a
Matt Loberg <mloberg@nerdery.com>
2019-04-04 09:56:06 -0500

    first

---
+++ Formula/dnsmasq-dnssec.rb
@@ -0,0 +1,85 @@
+class DnsmasqDnssec < Formula
+    desc "Lightweight DNS forwarder and DHCP server with DNSSEC support"
+    homepage "http://www.thekelleys.org.uk/dnsmasq/doc.html"
+    url "http://www.thekelleys.org.uk/dnsmasq/dnsmasq-2.80.tar.gz"
+    sha256 "9e4a58f816ce0033ce383c549b7d4058ad9b823968d352d2b76614f83ea39adc"
+  
+    depends_on "pkg-config" => :build
+    depends_on "nettle"
+
+    conflicts_with "dnsmasq", :because => "replaces dnsmasq"
+  
+    def install
+      ENV.deparallelize
+  
+      # Fix etc location
+      inreplace %w[dnsmasq.conf.example src/config.h man/dnsmasq.8
+                   man/es/dnsmasq.8 man/fr/dnsmasq.8].each do |s|
+        s.gsub! "/var/lib/misc/dnsmasq.leases",
+                var/"lib/misc/dnsmasq/dnsmasq.leases", false
+        s.gsub! "/etc/dnsmasq.conf", etc/"dnsmasq.conf", false
+        s.gsub! "/var/run/dnsmasq.pid", var/"run/dnsmasq/dnsmasq.pid", false
+        s.gsub! "/etc/dnsmasq.d", etc/"dnsmasq.d", false
+        s.gsub! "/etc/ppp/resolv.conf", etc/"dnsmasq.d/ppp/resolv.conf", false
+        s.gsub! "/etc/dhcpc/resolv.conf", etc/"dnsmasq.d/dhcpc/resolv.conf", false
+        s.gsub! "/usr/sbin/dnsmasq", HOMEBREW_PREFIX/"sbin/dnsmasq", false
+      end
+
+      # DNSSEC support
+      inreplace "src/config.h", "/* #define HAVE_DNSSEC */", "#define HAVE_DNSSEC"
+      inreplace "dnsmasq.conf.example" do |s|
+        s.gsub! "#conf-file=%%PREFIX%%/share/dnsmasq/trust-anchors.conf",
+                "conf-file=#{opt_pkgshare}/trust-anchors.conf"
+        s.gsub! "#dnssec", "dnssec"
+      end
+  
+      # Fix compilation on newer macOS versions.
+      ENV.append_to_cflags "-D__APPLE_USE_RFC_3542"
+  
+      inreplace "Makefile" do |s|
+        s.change_make_var! "CFLAGS", ENV.cflags
+        s.change_make_var! "LDFLAGS", ENV.ldflags
+      end
+  
+      system "make", "install", "PREFIX=#{prefix}"
+  
+      pkgshare.install "trust-anchors.conf"
+      etc.install "dnsmasq.conf.example" => "dnsmasq.conf"
+    end
+  
+    def post_install
+      (var/"lib/misc/dnsmasq").mkpath
+      (var/"run/dnsmasq").mkpath
+      (etc/"dnsmasq.d/ppp").mkpath
+      (etc/"dnsmasq.d/dhcpc").mkpath
+    end
+  
+    plist_options :startup => true
+  
+    def plist; <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_sbin}/dnsmasq</string>
+            <string>--keep-in-foreground</string>
+            <string>-C</string>
+            <string>#{etc}/dnsmasq.conf</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>KeepAlive</key>
+          <true/>
+        </dict>
+      </plist>
+    EOS
+    end
+  
+    test do
+      system "#{sbin}/dnsmasq", "--test"
+    end
+  end
---
+++ README.md
@@ -0,0 +1,16 @@
+# Homebrew Formula
+
+> Ravioli Ravioli, Give Me the Formuoli
+
+Homebrew Core removed arguments to improve installing formula. This is a
+collection of formula for arguments that were removed.
+
+## Usage
+
+[Tap](https://docs.brew.sh/Taps) this repository:
+
+    brew tap mloberg/formuoli
+
+Install the packages you need like normal
+
+    brew install dnsmasq-dnssec
